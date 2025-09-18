c14913f841623e81e5bdc50ccbf24ccb63d2d68b
JohnPaul Ortega <jpgortega10@gmail.com>
2025-09-18 10:48:17 -0500

    Add files via upload

---
+++ ArrayReversal.java
@@ -0,0 +1,36 @@
+//Array Reversal
+//reverse an input from the user by utilizing an array
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ArrayReversal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("How many numbers?");
+		int amountOfNumbers = sc.nextInt();
+		int[] arrayInput = new int[amountOfNumbers];
+		
+		System.out.println("Enter "+ amountOfNumbers + " numbers:");
+		for(int i = 0;i<amountOfNumbers;i++)
+			arrayInput[i] = sc.nextInt();
+		
+		int temp;
+		for(int i = 0;i<amountOfNumbers/2;i++)
+		{
+			temp = arrayInput[i];
+			arrayInput[i] = arrayInput[amountOfNumbers-1-i];
+			arrayInput[amountOfNumbers-1-i] = temp;
+		}
+		System.out.println("Reversed array:");
+		
+		for(int i=0;i<amountOfNumbers;i++)
+			System.out.print(arrayInput[i]+" ");
+	
+		sc.close();
+	}
+
+}
