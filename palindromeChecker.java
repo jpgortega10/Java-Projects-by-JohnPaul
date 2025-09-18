6a90dd71d919bc981fa273fcd6f60c11e1b5f14e
jpgortega10 <jpgortega10@gmail.com>
2025-09-17 22:12:13 -0500

    Update palindromeChecker.java

--- palindromeChecker.java
+++ palindromeChecker.java
@@ -3,7 +3,23 @@ import java.util.Scanner;
 public class palindromeChecker {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner io = new Scanner(System.in);
+		
+		System.out.println("Enter a word please:");
+		String word = io.nextLine();
+		for(int i =0;i<word.length();i++)
+		{
+			for(int j = word.length();j>0;j++)
+				if(word.charAt(i)==word.charAt(j))
+				{
+					System.out.println("palindrome");
+					break;
+				}
+		}
+		
+		
+		
+		
 
 	}
 
